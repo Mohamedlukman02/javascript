4ff544c68a5218325f0739e1bba1128cd837f888
Mohamed Lukman <mdlukman0204@gmail.com>
2026-07-27 21:10:41 +0530

    Add checks for number sign and day of the week

---
+++ Conditional_Statements.js
@@ -0,0 +1,43 @@
+//Check if a number is positive or negative//
+
+let num = -5;
+
+if (num > 0) {
+    console.log("Positive number");
+} else if (num < 0) {
+    console.log("Negative number");
+} else {
+    console.log("Zero");
+}
+
+//Switch to print days of the week//
+
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+//
